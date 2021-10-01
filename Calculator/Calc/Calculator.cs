13c4b54cd644371b3c11c9330939546fa9e365d8
Paul-kav <paulkavuma82@gmail.com>
2021-10-01 06:02:51 -0400

    Added calculator class

---
+++ Calculator/Calc/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyCalculator.Calc
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public static int Add(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = x + y;
+            return Answer;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyCalculator.Calc;
+
+namespace MyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Add(10, 100);
+            Console.WriteLine(result);
+        }
+    }
+}
