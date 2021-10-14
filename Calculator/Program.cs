30306a69e8309beb8fa14defd7fb52afd8809005
Paul-kav <paulkavuma82@gmail.com>
2021-10-14 04:06:35 -0400

    Added input text

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,8 +7,12 @@ namespace MyCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What type of calculation do you want to perform? (*, +, -)");
+            Console.ReadLine();
+            Console.WriteLine("Write the first number");
+            Console.ReadLine();
             int result = Calculator.Add(10, 100);
-            Console.WriteLine(result);
+           // Console.WriteLine(result);
         }
     }
 }
